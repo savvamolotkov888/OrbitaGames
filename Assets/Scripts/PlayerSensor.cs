7be70a8a7f0abba4e05e23dd3a66a0f5a39230cd
Savva <savvamolotkov888@gmail.com>
2023-01-27 11:36:52 +0300

    Refactoring

--- Assets/Scripts/PlayerSensor.cs
+++ Assets/Scripts/PlayerSensor.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+/// <summary>
+/// Датчики (поворот , высота от пола и т.д.)
+/// </summary>
 public class PlayerSensor : MonoBehaviour
 {
     public Transform Target;
-    public LayerMask playerMask;
     public RotateDirection rorator;
     private float angle;
     public float rayDist;
@@ -16,25 +18,26 @@ public class PlayerSensor : MonoBehaviour
 
     void FixedUpdate()
     {
-        Ray ray = new Ray(transform.position,-Vector3.up);
-        Debug.DrawLine(transform.position,-Vector3.up,Color.blue);
-        RaycastHit hit; 
-        
-        if (Physics.Raycast(ray, out hit,rayDist))
+        FloarCheck();
+        IceRotationCheck();
+    }
+    private void FloarCheck()
+    {
+        Ray ray = new Ray(transform.position, -Vector3.up);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, rayDist))
         {
 //            Debug.Log(hit.distance);
-            
+
             if (hit.distance < 0.5)
                 OnTheFloar = true;
             else
                 OnTheFloar = false;
-
-
-                ;
         }
-        
-            
-        
+    }
+    private void IceRotationCheck()
+    {
         angle = -Vector3.SignedAngle(Target.position - Ice.transform.position, Ice.transform.forward, Vector3.up);
         if (angle > 5 && angle < 180)
         {
