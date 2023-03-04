513c2f3dd568e295ffae4354b1c7b9667ef1b71c
Savva <savvamolotkov888@gmail.com>
2023-03-05 00:45:56 +0300

    Refactoring

--- Assets/OrbitaGames/Scripts/UI/PausePanel_Service.cs
+++ Assets/OrbitaGames/Scripts/UI/PausePanel_Service.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UI_Service : MonoBehaviour
+public class PausePanel_Service : MonoBehaviour
 {
     private InputSystem inputSystem;
 
