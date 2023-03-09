8c50786377bd8c5265a79e577bc4fc89d5392497
Savva <savvamolotkov888@gmail.com>
2023-03-09 19:42:25 +0300

    refactoring

--- Assets/OrbitaGames/Scripts/Player/IceDouble.cs
+++ Assets/OrbitaGames/Scripts/Player/IceDouble.cs
@@ -1,16 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class IceDouble : MonoBehaviour
 {
     [SerializeField] private Transform player;
+    [SerializeField] private Transform TargetVertical;
+    [SerializeField] private float DebugLength;
+
+    [SerializeField] private Transform X;
+    [SerializeField] private Transform Xm;
+    [SerializeField] private Transform Y;
+    [SerializeField] private Transform Ym;
+    [SerializeField] private Transform Z;
+    [SerializeField] private Transform Zm;
 
     // Update is called once per frame
     void Update()
     {
-        
-        transform.rotation = player.rotation * Quaternion.Euler(0,90,0);
-        Debug.DrawRay(transform.position, transform .position + transform.up*200, Color.white);
+        transform.rotation = player.rotation;
+
+        Debug.DrawRay(transform.position, X.position - transform.position, Color.white);
+        Debug.DrawRay(transform.position, Xm.position - transform.position, Color.red);
+        Debug.DrawRay(transform.position, Y.position - transform.position, Color.green);
+        Debug.DrawRay(transform.position, Ym.position - transform.position, Color.green);
+        Debug.DrawRay(transform.position, Z.position - transform.position, Color.blue);
+        Debug.DrawRay(transform.position, Zm.position - transform.position, Color.blue);
     }
 }
